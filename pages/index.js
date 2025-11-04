2b6ee7d4c52a5915f57b0e8dd50af011ae54e067
Erley Santana <112229186+FrsErley@users.noreply.github.com>
2025-11-04 12:51:53 +0000

    feat: subindo algumas coisas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Home() {
-    return <h1>Você é tchola</h1>
-}
+'use client'
 
-export default Home;
+export default function Home() {
+  const videoUrl = 'https://www.youtube.com/watch?v=IXdNnw99-Ic&list=RDIXdNnw99-Ic&start_radio=1'
+
+  const handleClick = () => {
+    window.open(videoUrl, '_blank')
+  }
+
+  return (
+    <div className="container">
+      <h1>Oi Duda (Aqui é o Shadow) 🐈‍⬛</h1>
+      <button onClick={handleClick}>Clique aqui</button>
+    </div>
+  )
+}
